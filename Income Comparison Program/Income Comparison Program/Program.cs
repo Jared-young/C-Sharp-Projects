7484a6eda8d17b501e5f4ffdf4cd306c987c24ec
Jared-young <54957290+Jared-young@users.noreply.github.com>
2023-10-18 15:56:10 -0400

    Assignment

---
+++ Income Comparison Program/Income Comparison Program/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.WriteLine("Anonymous Income Comparison Program\n");
+
+        //Person 1 Details
+        Console.WriteLine("Person 1");
+        int hourlyRate1 = GetHourlyRate("Hourly Rate");
+        int hoursWorkedPerWeek1 = GetHoursWorked("Hours worked per week");
+        int annualSalary1 = CalculateAnnualSalary(hourlyRate1, hoursWorkedPerWeek1);
+
+        //Person 2 Details
+        Console.WriteLine("\nPerson 2");
+        int hourlyRate2 = GetHourlyRate("Hourly Rate");
+        int hoursWorkedPerWeek2 = GetHoursWorked("Hours worked per week");
+        int annualSalary2 = CalculateAnnualSalary(hourlyRate2, hoursWorkedPerWeek2);
+
+        //Annual Salaries Listed
+        Console.WriteLine($"\nAnnual salary of Person 1: {annualSalary1:C}");
+        Console.ReadLine();
+        Console.WriteLine($"Annual salary of Person 2: {annualSalary2:C}");
+        Console.ReadLine();
+
+        // Compare salaries
+        bool person1EarnsMore = annualSalary1 > annualSalary2;
+        Console.WriteLine($"\nDoes Person 1 make more money than Person 2? {person1EarnsMore}");
+        Console.ReadLine();
+    }
+    int GetHourlyRate(string prompt)
+    {
+        Console.Write($"{prompt}: ");
+        return GetInputFromUser();
+    }
+    int GetHoursWorked(string prompt)
+    {
+        Console.Write($"{prompt}: ");
+        return GetInputFromUser();
+    }
+    int GetInputFromUser()
+    {
+        int input;
+        while (!int.TryParse(Console.ReadLine(), out input))
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number.");
+        }
+        return input;
+    }
+    int CalculateAnnualSalary(int hourlyRate, int hoursWorkedPerWeek)
+    {
+        return hourlyRate * hoursWorkedPerWeek * 52;
+    }
+}
+
