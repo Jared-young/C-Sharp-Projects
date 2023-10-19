32c9c2a31b6eebd5eec1eb5c69c65ce0232e5aae
Jared-young <54957290+Jared-young@users.noreply.github.com>
2023-10-18 21:12:03 -0400

    Car Insurance App

---
+++ Car Insurance Approval Program/Car Insurance Approval Program/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.Write("How old are you?");
+        int age = int.Parse(Console.ReadLine());
+
+        Console.Write("Have you ever had a DUI? (Answer with yes or no)");
+        string dui = Console.ReadLine();
+        bool yesDUI = dui == "yes";
+
+        Console.Write("How many speeding tickets do you have?");
+        int speedingTickets = int.Parse(Console.ReadLine());
+
+        bool driverQualified = (age >= 16) && !yesDUI && (speedingTickets <= 3);
+
+        Console.WriteLine("Qualified: " + driverQualified);
+        Console.ReadLine();
+    }
+    }
+
