6943fafcd9db4771968c9e722172e619f1471d5e
Jared-young <54957290+Jared-young@users.noreply.github.com>
2023-10-26 15:15:16 -0400

    Methods Assignment

---
+++ Calling Methods Assignment/Calling Methods Assignment/MathOperator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_Assignment
+{
+    public class MathOperations
+    {
+        public int Add(int number)
+        {
+            return number + 10;
+        }
+
+        public int Subtract(int number)
+        {
+            return number - 5;
+        }
+
+        public int Multiply(int number)
+        {
+            return number * 2;
+        }
+    }
+}
---
+++ Calling Methods Assignment/Calling Methods Assignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperations math = new MathOperations();
+
+            Console.Write("Enter a number: ");
+            if (int.TryParse(Console.ReadLine(), out int userNumber))
+            {
+                int resultAdd = math.Add(userNumber);
+                int resultSubtract = math.Subtract(userNumber);
+                int resultMultiply = math.Multiply(userNumber);
+
+                Console.WriteLine($"Result of adding 10: {resultAdd}");
+                Console.WriteLine($"Result of subtracting 5: {resultSubtract}");
+                Console.WriteLine($"Result of multiplying by 2: {resultMultiply}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
