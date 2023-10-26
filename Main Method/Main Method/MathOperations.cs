3fc1bdc168ea22edf47d417dc2d99d5b71707b45
Jared-young <54957290+Jared-young@users.noreply.github.com>
2023-10-26 17:37:05 -0400

    Main Method assignment

---
+++ Main Method/Main Method/MathOperations.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class MathOperations
+{
+    public int PerformMathOperation(int number, string operation)
+    {
+        int result = 0;
+
+        switch (operation)
+        {
+            case "add":
+                result = number + 10; 
+                break;
+            case "subtract":
+                result = number - 5;
+                break;
+            default:
+                Console.WriteLine("Invalid operation");
+                break;
+        }
+
+        return result;
+    }
+
+    public int PerformMathOperation(decimal number, string operation)
+    {
+        int result = 0;
+
+        switch (operation)
+        {
+            case "add":
+                result = (int)(number + 10);
+                break;
+            case "subtract":
+                result = (int)(number - 5);
+                break;
+            default:
+                Console.WriteLine("Invalid operation");
+                break;
+        }
+
+        return result;
+    }
+
+    public int PerformMathOperation(string number, string operation)
+    {
+        if (int.TryParse(number, out int parsedNumber))
+        {
+            return PerformMathOperation(parsedNumber, operation);
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Not a valid integer.");
+            return 0;
+        }
+    }
+}
---
+++ Main Method/Main Method/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method
+{
+    class Program
+    {
+        static void Main()
+        {
+            MathOperations mathOps = new MathOperations();
+
+            // Integer input
+            int intResult = mathOps.PerformMathOperation(15, "add");
+            Console.WriteLine("Result of addition: " + intResult);
+
+            // Decimal input
+            int decimalResult = mathOps.PerformMathOperation(8.5m, "subtract");
+            Console.WriteLine("Result of subtraction: " + decimalResult);
+
+            // String input 
+            int stringResult = mathOps.PerformMathOperation("20", "multiply");
+            Console.WriteLine("Result of multiplication: " + stringResult);
+
+            Console.ReadLine(); 
+        }
+    }
+}
