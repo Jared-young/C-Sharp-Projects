36a9ea28e88f450bdf1f05fc8acfe62fbc22447a
Jared-young <54957290+Jared-young@users.noreply.github.com>
2023-10-29 10:28:28 -0400

    Step 224

---
+++ Class Submission Step 224/Class Submission Step 224/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Submission_Step_224
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathClass mathInstance = new MathClass();
+
+            Console.Write("Enter a number: ");
+            int userInput = int.Parse(Console.ReadLine());
+
+            mathInstance.DivideByTwo(userInput);
+
+            mathInstance.DivideByTwo(userInput, out int result);
+            Console.WriteLine($"Result of division by 2 (using output parameter): {result}");
+
+            Console.ReadLine();
+        }
+    }
+}
