a81047a351552c2a4dd71810e13426df1ecca0ce
Jared-young <54957290+Jared-young@users.noreply.github.com>
2023-10-30 18:20:26 -0400

    Correction

--- Operators Assignment Step 253/Operators Assignment Step 253/Employee.cs
+++ Operators Assignment Step 253/Operators Assignment Step 253/Employee.cs
@@ -19,19 +19,14 @@ namespace Operators_Assignment_Step_253
 
         public static bool operator ==(Employee emp1, Employee emp2)
         {
-            if (ReferenceEquals(emp1, emp2))
-                return true;
-
-            if (emp1 is null || emp2 is null)
-                return false;
-
+         
             return emp1.Id == emp2.Id;
         }
 
 
         public static bool operator !=(Employee emp1, Employee emp2)
         {
-            return !(emp1 == emp2);
+            return emp1.Id != emp2.Id;
         }
     }
 }
