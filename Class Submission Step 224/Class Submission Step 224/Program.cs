b006fe1a635d63b5f847f1a52fad182f209c275e
Jared-young <54957290+Jared-young@users.noreply.github.com>
2023-10-30 17:11:29 -0400

    Corrections

--- Class Submission Step 224/Class Submission Step 224/Program.cs
+++ Class Submission Step 224/Class Submission Step 224/Program.cs
@@ -11,15 +11,16 @@ namespace Class_Submission_Step_224
         static void Main(string[] args)
         {
             MathClass mathInstance = new MathClass();
-
             Console.Write("Enter a number: ");
             int userInput = int.Parse(Console.ReadLine());
-
-            mathInstance.DivideByTwo(userInput);
-
             mathInstance.DivideByTwo(userInput, out int result);
             Console.WriteLine($"Result of division by 2 (using output parameter): {result}");
 
+            
+            Console.Write("Enter a number: ");
+            int userInput2 = int.Parse(Console.ReadLine());
+            StaticMethod.DivideByThree(userInput2, out int result2);
+            Console.WriteLine($"Result of division by 3 (using output parameter): {result2}");
             Console.ReadLine();
         }
     }
---
+++ Class Submission Step 224/Class Submission Step 224/Static Method.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Submission_Step_224
+{
+    public class StaticMethod
+    {
+        public static void DivideByThree(int number, out int result2)
+        {
+            result2 = number / 3;
+        }
+    }
+}
