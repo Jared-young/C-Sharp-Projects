2a14cbc007664b2243a545aaa42b395de7c7dd01
Jared-young <54957290+Jared-young@users.noreply.github.com>
2023-10-30 17:50:25 -0400

    Step 253

---
+++ Operators Assignment Step 253/Operators Assignment Step 253/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_Assignment_Step_253
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Employee(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public static bool operator ==(Employee emp1, Employee emp2)
+        {
+            if (ReferenceEquals(emp1, emp2))
+                return true;
+
+            if (emp1 is null || emp2 is null)
+                return false;
+
+            return emp1.Id == emp2.Id;
+        }
+
+
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            return !(emp1 == emp2);
+        }
+    }
+}
---
+++ Operators Assignment Step 253/Operators Assignment Step 253/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_Assignment_Step_253
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee(1, "Jared");
+            Employee employee2 = new Employee(2, "Lauren");
+            Employee employee3 = new Employee(1, "Jared");
+
+            Console.WriteLine("employee1 == employee2: " + (employee1 == employee2)); 
+            Console.WriteLine("employee1 == employee3: " + (employee1 == employee3));
+            Console.WriteLine("employee2 == employee3: " + (employee2 == employee3));
+
+            Console.ReadLine();
+        }
+    }
+}
