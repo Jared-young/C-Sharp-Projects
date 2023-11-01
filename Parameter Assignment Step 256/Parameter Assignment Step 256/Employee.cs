dbdf06cc537433b1016da971e08edc14032452f7
Jared-young <54957290+Jared-young@users.noreply.github.com>
2023-11-01 19:01:43 -0400

    Step 256

---
+++ Parameter Assignment Step 256/Parameter Assignment Step 256/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameter_Assignment_Step_256
+{
+    class Employee<T>
+    {
+        public List<T> Things { get; set; }
+
+        public Employee(List<T> things)
+        {
+            Things = things;
+        }
+    }
+}
---
+++ Parameter Assignment Step 256/Parameter Assignment Step 256/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Parameter_Assignment_Step_256
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employee1 = new Employee<string>(new List<string> { "Laptop", "Backpack", "Phone" });
+
+            var employee2 = new Employee<int>(new List<int> { 1, 2, 3, 4, 5 });
+
+            Console.WriteLine("Employee 1's Things (strings):");
+            foreach (var item in employee1.Things)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("\nEmployee 2's Things (integers):");
+            foreach (var item in employee2.Things)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
