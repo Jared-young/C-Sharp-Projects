8247839a84b05d39b86de01b2c5fd73a98971ac7
Jared-young <54957290+Jared-young@users.noreply.github.com>
2023-11-01 21:35:08 -0400

    262

---
+++ Parsing Enums Assignment Step 262/Parsing Enums Assignment Step 262/DayOfWeek.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parsing_Enums_Assignment_Step_262
+{
+    public class DayOfWeekEnum
+    {
+        public enum DayOfWeekEnum2
+        {
+            Sunday, 
+            Monday, 
+            Tuesday, 
+            Wednesday, 
+            Thursday,
+            Friday, 
+            Saturday
+        }
+    }
+}
---
+++ Parsing Enums Assignment Step 262/Parsing Enums Assignment Step 262/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parsing_Enums_Assignment_Step_262
+{
+    public enum DayOfWeekEnum
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter the current day of the week: ");
+                string input = Console.ReadLine();
+
+                DayOfWeekEnum currentDay;
+                if (Enum.TryParse(input, true, out currentDay))
+                {
+                    Console.WriteLine($"You entered: {currentDay}");
+                }
+                else
+                {
+                    Console.WriteLine("Please enter an actual day of the week.");
+                }
+            }
+            catch (Exception example)
+            {
+                Console.WriteLine("An Error Occured: " + example.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
