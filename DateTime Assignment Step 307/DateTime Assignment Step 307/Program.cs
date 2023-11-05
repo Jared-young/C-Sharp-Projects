286c79ad04dc97bfb7a865db660203b506700759
Jared-young <54957290+Jared-young@users.noreply.github.com>
2023-11-05 15:20:13 -0500

    Step 307

---
+++ DateTime Assignment Step 307/DateTime Assignment Step 307/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime_Assignment_Step_307
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentDateTime = DateTime.Now;
+            Console.WriteLine("Current date and time:" + currentDateTime);
+
+            Console.Write("Enter a number of hours: ");
+            if (int.TryParse(Console.ReadLine(), out int hoursToAdd))
+            {
+                DateTime futureDateTime = currentDateTime.AddHours(hoursToAdd);
+                Console.WriteLine($"Time in {hoursToAdd} hours will be: {futureDateTime}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number of hours.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
